3be63015673acdafadb1a5b7f67a638ba6d8154a
Latha Kumarasamy Sakthivel <lathasowdi@gmail.com>
2020-09-28 15:55:23 +0200

    Add project files.

---
+++ JoinOefening/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace JoinOefening
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.cbpersonen = new System.Windows.Forms.ComboBox();
+            this.btnnaarJoske = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VOORNAAM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ACHTERNAAM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "LEEFTIJD";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblId);
+            this.groupBox1.Controls.Add(this.cbpersonen);
+            this.groupBox1.Controls.Add(this.btnnaarJoske);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(27, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(222, 248);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PERSOON";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(138, 118);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(35, 13);
+            this.lblId.TabIndex = 11;
+            this.lblId.Text = "label5";
+            // 
+            // cbpersonen
+            // 
+            this.cbpersonen.FormattingEnabled = true;
+            this.cbpersonen.Location = new System.Drawing.Point(9, 110);
+            this.cbpersonen.Name = "cbpersonen";
+            this.cbpersonen.Size = new System.Drawing.Size(121, 21);
+            this.cbpersonen.TabIndex = 10;
+            this.cbpersonen.SelectedIndexChanged += new System.EventHandler(this.cbpersonen_SelectedIndexChanged);
+            // 
+            // btnnaarJoske
+            // 
+            this.btnnaarJoske.Location = new System.Drawing.Point(22, 219);
+            this.btnnaarJoske.Name = "btnnaarJoske";
+            this.btnnaarJoske.Size = new System.Drawing.Size(172, 23);
+            this.btnnaarJoske.TabIndex = 9;
+            this.btnnaarJoske.Text = "VERANDEREN NAAR JOSKE";
+            this.btnnaarJoske.UseVisualStyleBackColor = true;
+            this.btnnaarJoske.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "BEWERKEN";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(116, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "VERWIJDEREN";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "TOEVOEGEN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(94, 74);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(298, 30);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(212, 100);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "BADGE";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(106, 68);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "VERWIJDEREN";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "TOEVOEGEN";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(80, 26);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "OPSCHRIFT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnnaarJoske;
+        private System.Windows.Forms.ComboBox cbpersonen;
+        private System.Windows.Forms.Label lblId;
+    }
+}
+
---
+++ JoinOefening/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JoinOefening
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(JoinedDatabaseEntities ctx= new JoinedDatabaseEntities())
+            {
+                string voornaam = "";
+                if (textBox1.Text.Trim() != "")
+                {
+                    voornaam = textBox1.Text.Trim();
+                }
+                else
+                {
+                    MessageBox.Show("Geef een Voornaam a.u.b");
+                }
+                string achternaam = "";
+                if (textBox2.Text.Trim() != "")
+                {
+                    achternaam = textBox2.Text.Trim();
+                }
+                else
+                {
+                    MessageBox.Show("Geef een Achternaam a.u.b");
+                }
+                
+                int leeftijd;
+                leeftijd = (int)numericUpDown1.Value;
+                
+                ctx.Persoons.Add(new Persoon() { Voornaam = voornaam, Achternaam = achternaam, Leeftijd = leeftijd });
+                ctx.SaveChanges();
+                MessageBox.Show("Persoon Toevoegd");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (JoinedDatabaseEntities ctx = new JoinedDatabaseEntities())
+            {
+                var query = ctx.Persoons.Select(x => x);
+        BindingList<Persoon> listItems = new BindingList<Persoon>();
+                cbpersonen.DisplayMember = "Voornaam";
+                cbpersonen.ValueMember = "PersoonID";
+        foreach (var item in query)
+                {
+                    Persoon persoon = new Persoon()
+                    {
+                        PersoonID = item.PersoonID,
+                        Voornaam = item.Voornaam,
+                    };
+            listItems.Add(persoon);
+                }
+                cbpersonen.DataSource = listItems;
+            }
+
+        }
+
+        private void cbpersonen_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           // lblId.Text = cbpersonen.SelectedValue.ToString();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int value = Convert.ToInt32(cbpersonen.SelectedValue);
+            using (JoinedDatabaseEntities ctx = new JoinedDatabaseEntities())
+            {
+                ctx.Persoons.Where(p => p.PersoonID == value).FirstOrDefault().Voornaam = "Joske";
+                ctx.SaveChanges();
+            }
+
+        }
+    }
+}
